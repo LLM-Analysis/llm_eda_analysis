40b5e1f38370f6e99596e6ad96fdb3dd9501b9dd
LLMMonitor <llmwebappmonitor@gmail.com>
2024-09-21 16:53:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ if file is not None:
             except Exception as e:
                 if "chart_response" in st.session_state:
                     del st.session_state["chart_response"]
-    
+                st.write(e)
                 st.session_state['re-attempt'] += 1
                 st.session_state["progress_text"] = {"val": 50, 
                                                      "text": f"{st.session_state['re-attempt']} attempt(s) to regenerate the charts"}
